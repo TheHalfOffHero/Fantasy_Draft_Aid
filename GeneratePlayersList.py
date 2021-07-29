9ffdb1671bc95d9099192f95aa13e545fa20b256
mruiz <mfruiz33@gmail.com>
2021-07-28 20:16:21 -0400

    Player and Draft classes working as intended, added generatePlayerList to make list of player objects and way to print that list from object

--- GeneratePlayersList.py
+++ GeneratePlayersList.py
@@ -1,17 +1,9 @@
 import csv
-#Might use pandas? csv module is in every python 3 installation. Might be easier
-
-#import pandas as pd
-#df=pd.read_csv('TenTeamStandardBeerSheet.csv', encoding='us-ascii');
-#print(df.to_string());
-
-# We want to read int he csv and instantiate a list of Player Objects so we can manipulate as necessary
-# columns: Name,Pos,Tm/Bye,Average,Stdev,Rank,Tier,ECR,ECRAvg,ECR VS. ADP,PS,Baseline
 
 #Player class
 #Takes columns from csv and creates objects
 class Player:
-    def __init__(self, Name, Pos, Tm_and_Bye, Average, Stdev, Rank, Tier, ECR, ECRAvgVSAdp,PS):
+    def __init__(self, Name, Pos, Tm_and_Bye, Average, Stdev, Rank, Tier, ECR, ECRAvgVSAdp,PS): #Not sure what PS is so its not included in Player Object for now
         self.Name = Name
         self.Pos = Pos
         self.Tm_and_Bye = Tm_and_Bye
@@ -23,22 +15,35 @@ class Player:
         self.ECRAvgVSAdp = ECRAvgVSAdp
 
 
+#Draft Class will include all draftable players with all values in csv included, with the exception of the last one.
 class Draft:
     def __init__(self, csv):
         self.csv = csv
         self.PlayerList = []
 
+    #GeneratePlayerList creates reader and line by line adds them to self.PlayerList property
+    #This allows me to manipulate and generate other lists with all available Players
+    #accessed as draftObject.generatePlayerList()
     def generatePlayerList(self):
         with open(self.csv, 'r') as file:
             reader = csv.reader(file)
             for row in reader:
-                print(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8])
-                self.PlayerList.append(Player(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]))
+                self.PlayerList.append(Player(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9]))
 
+    #written just to test that the list was actually generating
+    def printPlayerList(self):
+        for i in self.PlayerList:
+            print(i.Name + ', ' + i.Pos)
+
+
+
+#Uncomment to test that logic works
 #test = Draft('TenTeamStandardBeerSheet.csv')
-#test.generatePlayerList
-#print(test.PlayerList)
+#test.generatePlayerList()
+#test.printPlayerList()
 
+#testing Player
 #This works just fine but for some reason generatePlayerList gives me an empty list printed
-testPlayer = Player('Matt', 'QB', 'TB/3', 2, 3, 1, 1, 1, 1, 1)
-print(testPlayer.Name, testPlayer.Pos)
+#testPlayer = Player('Matt', 'QB', 'TB/3', 2, 3, 1, 1, 1, 1, 1)
+#print(testPlayer.Name, testPlayer.Pos)
+
